89cc6509caca4707b46cb5ec0d4745365bfafa1c
Bruno Aurelio <baurelio.rodrigues@gmail.com>
2026-02-19 21:01:17 -0300

    fix-change url docussaurus

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://brunoaurelio89.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'https://github.com/brunoaurelio89/docussaurus/',
+  baseUrl: '/docussaurus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
